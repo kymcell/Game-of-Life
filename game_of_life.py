a8e73ecf6607133f7b9f341eafd193b2f501585c
Kaelan <engholdt8911@uwlax.edu>
2019-02-26 22:18:19 -0600

    initial commit

---
+++ game_of_life.py
@@ -0,0 +1,13 @@
+#
+# CS 224 Spring 2019
+# Semester Project
+#
+# Description
+#
+# Authors: Kaelan Engholdt, Garrett Kern, Kyle McElligott
+# Date: 2/25/2019
+#
+
+# imports
+
+print "Hello World!"
