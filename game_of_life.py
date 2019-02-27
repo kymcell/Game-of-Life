5e426bbb335d0f9ae73c305107567922839276b1
Kaelan <engholdt8911@uwlax.edu>
2019-02-27 17:34:14 -0600

    TODO for 2/27/2019

--- game_of_life.py
+++ game_of_life.py
@@ -10,4 +10,11 @@
 
 # imports
 
+
 print "Hello World!"
+
+
+# TODO :
+#  Determine ruleset functions (different logic functions)
+#  How will user determine whicch ruleset to use?
+#  Which PyGame module/s are best and needed for this project?
